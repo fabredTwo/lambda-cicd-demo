67b6f0d6a9e1baf7b044e737cfb13fcbe3afbc99
fabredTwo <fabred2@hotmail.com>
2026-01-26 16:44:00 -0500

    listo cambia el app.py

--- app.py
+++ app.py
@@ -1,19 +1,36 @@
 from suma import sumar
 import os
 import sys
+import json
 
 print("=== LAMBDA DEBUG START ===")
 print("cwd:", os.getcwd())
 print("files:", os.listdir("."))
 print("sys.path:", sys.path)
 print("=== LAMBDA DEBUG END ===")
+
 def lambda_handler(event, context):
-    a = event.get("a", 0)
-    b = event.get("b", 0)
+    params = event.get("queryStringParameters") or {}
+
+    try:
+        a = int(params.get("a"))
+        b = int(params.get("b"))
+    except (TypeError, ValueError):
+        return {
+            "statusCode": 400,
+            "body": json.dumps({
+                "error": "Parámetros inválidos. Usa ?a=NUM&b=NUM"
+            })
+        }
 
     resultado = sumar(a, b)
 
     return {
         "statusCode": 200,
-        "result": resultado
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "resultado": resultado
+        })
     }
